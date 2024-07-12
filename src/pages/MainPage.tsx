d89b928372f52a9e169a0d418f987598656d96bc
kim-minkyoung <decomin02@pusan.ac.kr>
2024-07-12 19:52:19 +0900

    feat: tabBar 3개 라우팅

---
+++ src/pages/MainPage.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Tab1: React.FC = () => {
+  return (
+    <div>
+      <p>
+        이것은 <span className="font-semibold text-gray-800">첫 번째</span> 탭의
+        내용입니다.
+      </p>
+    </div>
+  );
+};
+
+export default Tab1;
---
+++ src/pages/PeoplePage.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Tab2: React.FC = () => {
+  return (
+    <div>
+      <p>
+        이것은 <span className="font-semibold text-gray-800">두 번째</span> 탭의
+        내용입니다.
+      </p>
+    </div>
+  );
+};
+
+export default Tab2;
