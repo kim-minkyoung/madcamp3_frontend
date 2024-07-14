1fc0da464fe9e94c4f7ed9fc52f4902cf250574d
kim-minkyoung <decomin02@pusan.ac.kr>
2024-07-14 17:12:13 +0900

    feat: 채팅 ui 구현

--- src/pages/rooms/RoomPage.tsx
+++ src/pages/rooms/RoomPage.tsx
@@ -9,6 +9,11 @@ const RoomPage: React.FC = () => {
   const [remoteStream, setRemoteStream] = useState<MediaStream | null>(null);
   const webSocketRef = useRef<WebSocket | null>(null);
   const peerConnectionRef = useRef<RTCPeerConnection | null>(null);
+  const [messages, setMessages] = useState<{ sender: string; text: string }[]>(
+    []
+  );
+  const [inputMessage, setInputMessage] = useState("");
+  const userId = useRef<string>(Math.random().toString(36).substring(7));
 
   useEffect(() => {
     const init = async () => {
@@ -27,21 +32,20 @@ const RoomPage: React.FC = () => {
           const message = JSON.parse(event.data);
           console.log("Received raw message:", event.data);
           console.log("Parsed message:", message);
+          console.log("message.userId.current:", message.userId.current);
+          console.log("userId.current:", userId.current);
 
-          if (message.action === "sendMessage") {
-            handleChatMessage(message.message);
+          if (
+            message.action === "chat-message" &&
+            message.userId.current !== userId.current
+          ) {
+            handleChatMessage({ sender: "you", text: message.message.text });
           }
 
           // Handle other types of messages as needed
         } catch (error) {
           console.error("Error parsing JSON:", error);
         }
-      };
-
-      const handleChatMessage = (message: string) => {
-        console.log(`Received chat message: ${message}`);
-        // 여기에 채팅 메시지를 UI에 추가하는 로직을 추가하세요.
-        // 예를 들어, 메시지를 화면에 출력하거나 채팅 UI 컴포넌트를 업데이트할 수 있습니다.
       };
 
       socket.onclose = () => {
@@ -127,61 +131,112 @@ const RoomPage: React.FC = () => {
     };
   }, [roomId]);
 
+  const handleChatMessage = (message: { sender: string; text: string }) => {
+    console.log(`Received chat message: ${message.text}`);
+    setMessages((prevMessages) => [...prevMessages, message]);
+  };
+
+  const sendMessage = () => {
+    if (webSocketRef.current?.readyState === WebSocket.OPEN) {
+      const message = inputMessage.trim();
+      if (message) {
+        const chatMessage = { sender: "Me", text: message };
+        webSocketRef.current.send(
+          JSON.stringify({
+            action: "sendmessage",
+            roomId,
+            userId,
+            message: chatMessage,
+          })
+        );
+        setMessages((prevMessages) => [...prevMessages, chatMessage]);
+        setInputMessage("");
+      }
+    }
+  };
+
   return (
-    <div>
-      <h2>Room {roomId} Video Call</h2>
-      <div>
-        {remoteStream && (
-          <video
-            ref={remoteVideoRef}
-            autoPlay
-            playsInline
-            className="transform-none"
-          ></video>
-        )}
-        {localStream && (
-          <video
-            ref={localVideoRef}
-            autoPlay
-            playsInline
-            muted
-            className="transform-none"
-          ></video>
-        )}
-      </div>
-      <div>
-        <input type="text" id="chatInput" />
-        <button
-          onClick={() => {
-            if (webSocketRef.current?.readyState === WebSocket.OPEN) {
-              const message = (
-                document.getElementById("chatInput") as HTMLInputElement
-              ).value.trim();
-              if (message) {
-                webSocketRef.current.send(
-                  JSON.stringify({
-                    action: "sendmessage",
-                    roomId,
-                    message,
-                  })
-                );
-                console.log(`Sent chat message: ${message}`);
-              }
-            }
-          }}
-        >
-          Send
-        </button>
-      </div>
-      <button
-        onClick={() => {
-          if (webSocketRef.current?.readyState === WebSocket.OPEN) {
-            webSocketRef.current.close();
-          }
+    <div style={{ display: "flex", flexDirection: "row", height: "100vh" }}>
+      <div
+        style={{
+          flex: 1,
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
         }}
       >
-        End Call
-      </button>
+        <h2>Room {roomId} Video Call</h2>
+        <div style={{ display: "flex", flexDirection: "column", flex: 1 }}>
+          {remoteStream && (
+            <video
+              ref={remoteVideoRef}
+              autoPlay
+              playsInline
+              className="transform-none"
+              style={{ width: "100%", height: "50%" }}
+            ></video>
+          )}
+          {localStream && (
+            <video
+              ref={localVideoRef}
+              autoPlay
+              playsInline
+              muted
+              className="transform-none"
+              style={{ width: "100%", height: "50%" }}
+            ></video>
+          )}
+        </div>
+      </div>
+      <div className="flex flex-col h-screen">
+        <div className="flex-grow p-4 overflow-auto">
+          {messages.map((message, index) => (
+            <div
+              key={index}
+              className={`flex ${
+                message.sender === "Me" ? "justify-end" : "justify-start"
+              } mb-4`}
+            >
+              {message.sender !== "Me" && (
+                <img
+                  src="https://source.unsplash.com/random/50x50"
+                  className="object-cover w-8 h-8 rounded-full"
+                  alt=""
+                />
+              )}
+              <div
+                className={`py-3 px-4 rounded-3xl text-white ${
+                  message.sender === "Me"
+                    ? "bg-blue-400 rounded-bl-3xl rounded-tl-3xl rounded-tr-xl"
+                    : "bg-gray-400 rounded-br-3xl rounded-tr-3xl rounded-tl-xl ml-2"
+                }`}
+              >
+                {message.text}
+              </div>
+              {message.sender === "Me" && (
+                <img
+                  src="https://source.unsplash.com/random/50x50"
+                  className="object-cover w-8 h-8 rounded-full"
+                  alt=""
+                />
+              )}
+            </div>
+          ))}
+        </div>
+        <div className="p-4 bg-gray-300">
+          <input
+            className="w-full px-3 py-2 rounded-xl"
+            type="text"
+            placeholder="type your message here..."
+            value={inputMessage}
+            onChange={(e) => setInputMessage(e.target.value)}
+            onKeyPress={(e) => e.key === "Enter" && sendMessage()}
+          />
+          <button className="hidden" onClick={sendMessage}>
+            Send
+          </button>
+        </div>
+      </div>
     </div>
   );
 };
